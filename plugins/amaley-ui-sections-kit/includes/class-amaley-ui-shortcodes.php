289f7bab4c2b82c60b4459a56e9d8e0420361d28
praveen-de-reptoiur <praveenraaj966@gmail.com>
2026-05-28 15:35:30 +0530

    Register page trust strip shortcode aliases

--- plugins/amaley-ui-sections-kit/includes/class-amaley-ui-shortcodes.php
+++ plugins/amaley-ui-sections-kit/includes/class-amaley-ui-shortcodes.php
@@ -22,6 +22,8 @@ final class Amaley_UI_Shortcodes {
 		add_shortcode( 'amaley_button', array( 'Amaley_UI_Button', 'render' ) );
 		add_shortcode( 'amaley_button_group', array( 'Amaley_UI_Button_Group', 'render' ) );
 		add_shortcode( 'amaley_trust_item', array( 'Amaley_UI_Trust_Item', 'render' ) );
+		add_shortcode( 'amaley_page_trust_strip', array( 'Amaley_UI_Trust_Strip', 'render' ) );
+		add_shortcode( 'amaley_trust_strip', array( 'Amaley_UI_Trust_Strip', 'render' ) );
 		add_shortcode( 'amaley_brand_promise', array( 'Amaley_UI_Brand_Promise', 'render' ) );
 		add_shortcode( 'amaley_cta_band', array( 'Amaley_UI_CTA_Band', 'render' ) );
 		add_shortcode( 'amaley_empty_state', array( 'Amaley_UI_Empty_State', 'render' ) );
